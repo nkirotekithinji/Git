742be8b524404fb9809ad6bf554182b477fcf6a5
Florence Nkirote <nkiroteflorence85@gmail.com>
2020-01-15 12:38:26 +0300

    first commit

---
+++ user.py
@@ -0,0 +1,43 @@
+[
+    {
+      "user_id": 4288,
+      "user_system_id": "bc943535-7c3f-4c08-ab02-89528408c3e8",
+      "user_created_at": "2019-09-25T14:35:21.820292+00:00",
+      "user_updated_at": "2019-12-19T13:39:22.843047+00:00",
+      "user_is_active": true,
+      "user_data_bag": {
+        "lang": "es-MX",
+        "gender": "male",
+        "age_group": "3",
+        "last_name": "Orenge",
+        "first_name": "Antony",
+        "submitted_feedback": [
+          "moja-feedback"
+        ]
+      },
+      "prev": 4288,
+      "next": 4289,
+      "mac_addresses": [
+        {
+          "mac_address_id": 6,
+          "mac_address_user_id": 4288,
+          "mac_address_mac_address": "a0:c9:a0:ea:7f:e6",
+          "mac_address_is_active": true,
+          "mac_address_created_at": "2019-09-25T14:35:21.822720+00:00",
+          "mac_address_updated_at": "2019-09-25T14:35:21.822720+00:00"
+        }
+      ],
+      "phone_numbers": [
+        {
+          "phone_number_id": 93,
+          "phone_number_user_id": 4288,
+          "phone_number_msisdn": "+254722112704",
+          "phone_number_is_active": true,
+          "phone_number_is_verified": true,
+          "phone_number_otp": "56626ff45dd863da4148044fe8f8dd459490a17b4fbb835be9408bde0e0fa3c1",
+          "phone_number_created_at": "2019-10-04T07:04:28.620900+00:00",
+          "phone_number_updated_at": "2019-11-08T13:49:42.911813+00:00"
+        }
+      ]
+    }
+  ]
